26da21afb1befd0fc5c36f1ef6734ee02695fab9
Stephanie Hutchinson <stephehutch@gmail.com>
2019-02-05 19:42:30 -0800

    increaded scores

---
+++ models/seeds.sql
@@ -0,0 +1,85 @@
+USE DATABASE game_db
+
+INSERT INTO players (userName, highsSore, photoUrl, dogOrCat, city)
+VALUES ("Kitty", 56768, "https://cdn3.vectorstock.com/i/1000x1000/01/77/businesswoman-character-avatar-icon-vector-12800177.jpg", "cat", "Seattle");
+
+INTO players (userName, highsSore, photoUrl, dogOrCat, city)
+VALUES ("Tom", 97366, "http://chittagongit.com//images/avatar-icon/avatar-icon-4.jpg", "cat", "Lynnwood");
+
+INSERT INTO players (userName, highsSore, photoUrl, dogOrCat, city)
+VALUES ("Kitty", 45656, "https://cdn3.vectorstock.com/i/1000x1000/01/77/businesswoman-character-avatar-icon-vector-12800177.jpg", "dog", "Seattle");
+
+INTO players (userName, highsSore, photoUrl, dogOrCat, city)
+VALUES ("Tom", 9235, "http://chittagongit.com//images/avatar-icon/avatar-icon-4.jpg", "cat", "Lynnwood");
+
+INSERT INTO players (userName, highsSore, photoUrl, dogOrCat, city)
+VALUES ("Kitty", 8468, "https://cdn3.vectorstock.com/i/1000x1000/01/77/businesswoman-character-avatar-icon-vector-12800177.jpg", "cat", "Seattle");
+
+INTO players (userName, highsSore, photoUrl, dogOrCat, city)
+VALUES ("Tom", 9996, "http://chittagongit.com//images/avatar-icon/avatar-icon-4.jpg", "cat", "Lynnwood");
+
+INSERT INTO players (userName, highsSore, photoUrl, dogOrCat, city)
+VALUES ("Kitty", 26734, "https://cdn3.vectorstock.com/i/1000x1000/01/77/businesswoman-character-avatar-icon-vector-12800177.jpg", "dog", "Seattle");
+
+INTO players (userName, highsSore, photoUrl, dogOrCat, city)
+VALUES ("Tom", 26790, "http://chittagongit.com//images/avatar-icon/avatar-icon-4.jpg", "cat", "Lynnwood");
+
+INSERT INTO players (userName, highsSore, photoUrl, dogOrCat, city)
+VALUES ("Kitty", 24676, "https://cdn3.vectorstock.com/i/1000x1000/01/77/businesswoman-character-avatar-icon-vector-12800177.jpg", "dog", "Seattle");
+
+INTO players (userName, highsSore, photoUrl, dogOrCat, city)
+VALUES ("Hannah", 56687, "http://chittagongit.com//images/avatar-icon/avatar-icon-4.jpg", "cat", "Lynnwood");
+
+INSERT INTO players (userName, highsSore, photoUrl, dogOrCat, city)
+VALUES ("Lue", 23085, "https://cdn3.vectorstock.com/i/1000x1000/01/77/businesswoman-character-avatar-icon-vector-12800177.jpg", "dog", "Seattle");
+
+INTO players (userName, highsSore, photoUrl, dogOrCat, city)
+VALUES ("Luke", 34678, "http://chittagongit.com//images/avatar-icon/avatar-icon-4.jpg", "cat", "Lynnwood");
+
+INSERT INTO players (userName, highsSore, photoUrl, dogOrCat, city)
+VALUES ("Ned", 8967, "https://cdn3.vectorstock.com/i/1000x1000/01/77/businesswoman-character-avatar-icon-vector-12800177.jpg", "cat", "Seattle");
+
+INTO players (userName, highsSore, photoUrl, dogOrCat, city)
+VALUES ("Simo", 46342, "http://chittagongit.com//images/avatar-icon/avatar-icon-4.jpg", "cat", "Lynnwood");
+
+INSERT INTO players (userName, highsSore, photoUrl, dogOrCat, city)
+VALUES ("Soto", 76828, "https://cdn3.vectorstock.com/i/1000x1000/01/77/businesswoman-character-avatar-icon-vector-12800177.jpg", "cat", "Seattle");
+
+INTO players (userName, highsSore, photoUrl, dogOrCat, city)
+VALUES ("Tom", 45583, "http://chittagongit.com//images/avatar-icon/avatar-icon-4.jpg", "cat", "Lynnwood");
+
+INSERT INTO players (userName, highsSore, photoUrl, dogOrCat, city)
+VALUES ("Kitty", 28276, "https://cdn3.vectorstock.com/i/1000x1000/01/77/businesswoman-character-avatar-icon-vector-12800177.jpg", "cat", "Seattle");
+
+INTO players (userName, highsSore, photoUrl, dogOrCat, city)
+VALUES ("Jose", 12369, "http://chittagongit.com//images/avatar-icon/avatar-icon-4.jpg", "cat", "Lynnwood");
+
+INSERT INTO players (userName, highsSore, photoUrl, dogOrCat, city)
+VALUES ("Nikki", 9877, "https://cdn3.vectorstock.com/i/1000x1000/01/77/businesswoman-character-avatar-icon-vector-12800177.jpg", "cat", "Seattle");
+
+INTO players (userName, highsSore, photoUrl, dogOrCat, city)
+VALUES ("Ren", 94372, "http://chittagongit.com//images/avatar-icon/avatar-icon-4.jpg", "cat", "Lynnwood");
+
+INSERT INTO players (userName, highsSore, photoUrl, dogOrCat, city)
+VALUES ("Juan", 9708, "https://cdn3.vectorstock.com/i/1000x1000/01/77/businesswoman-character-avatar-icon-vector-12800177.jpg", "cat", "Seattle");
+
+INTO players (userName, highsSore, photoUrl, dogOrCat, city)
+VALUES ("Pham", 6789, "http://chittagongit.com//images/avatar-icon/avatar-icon-4.jpg", "cat", "Lynnwood");
+
+INSERT INTO players (userName, highsSore, photoUrl, dogOrCat, city)
+VALUES ("Lesli", 20469, "https://cdn3.vectorstock.com/i/1000x1000/01/77/businesswoman-character-avatar-icon-vector-12800177.jpg", "dog", "Seattle");
+
+INTO players (userName, highsSore, photoUrl, dogOrCat, city)
+VALUES ("Amro", 16767, "http://chittagongit.com//images/avatar-icon/avatar-icon-4.jpg", "cat", "Lynnwood");
+
+INSERT INTO players (userName, highsSore, photoUrl, dogOrCat, city)
+VALUES ("Mehmed", 78589, "https://cdn3.vectorstock.com/i/1000x1000/01/77/businesswoman-character-avatar-icon-vector-12800177.jpg", "cat", "Seattle");
+
+INTO players (userName, highsSore, photoUrl, dogOrCat, city)
+VALUES ("Tomoyo", 56697, "http://chittagongit.com//images/avatar-icon/avatar-icon-4.jpg", "cat", "Lynnwood");
+
+INSERT INTO players (userName, highsSore, photoUrl, dogOrCat, city)
+VALUES ("Kim", 34465, "https://cdn3.vectorstock.com/i/1000x1000/01/77/businesswoman-character-avatar-icon-vector-12800177.jpg", "dog", "Seattle");
+
+INTO players (userName, highsSore, photoUrl, dogOrCat, city)
+VALUES ("Mike", 54369, "http://chittagongit.com//images/avatar-icon/avatar-icon-4.jpg", "cat", "Lynnwood");
